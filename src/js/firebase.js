ad37f523c9204b4dd4613874322de3fcad613c06
robertinho33 <sou_roberto@hotmail.com>
2025-04-03 23:45:07 -0300

    x2

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -11,14 +11,15 @@ import {
     where 
 } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyB3785y6GPsFH7xuwfwjcBoPjvUfE3kSMw",
-    authDomain: "alphaglamstart.firebaseapp.com",
-    projectId: "alphaglamstart",
-    storageBucket: "alphaglamstart.appspot.com",
-    messagingSenderId: "885178660314",
-    appId: "1:885178660314:web:1bb3a78be9fa7fdcdefce3"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyCVenp_Ev_M0cPa_IGoEuPDNBfgz26dAUU",
+    authDomain: "novo-b4adb.firebaseapp.com",
+    projectId: "novo-b4adb",
+    storageBucket: "novo-b4adb.firebasestorage.app",
+    messagingSenderId: "578134453072",
+    appId: "1:578134453072:web:1238e881ae7d83b89b9a55",
+    measurementId: "G-VH9CKYTS9N"
+  };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
