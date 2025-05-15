e9770f77abe0aef5be7a5164c5dc88f094f141db
robertinho33 <sou_roberto@hotmail.com>
2025-05-15 07:15:09 -0300

    za

--- js/loadMenu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('../partials/menu.html')
-        .then(response => response.text())
-        .then(data => document.getElementById('menu').innerHTML = data);
-    
-    fetch('../partials/footer.html')
-        .then(response => response.text())
-        .then(data => document.getElementById('footer').innerHTML = data);
-});
--- js/utils.js
+++
@@ -1,3 +0,0 @@
-const Utils = {
-    // Adicionar funções utilitárias no futuro
-};
