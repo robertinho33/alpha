631a17592555d72b9891d8a5fcce622f71c781e7
robertinho33 <sou_roberto@hotmail.com>
2025-05-18 09:14:56 -0300

    jk

--- js/auth.js
+++
@@ -1,9 +0,0 @@
-// auth.js
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js';
-import firebaseConfig from './firebaseConfig.js';
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged };
--- js/firestore.js
+++
@@ -1,9 +0,0 @@
-// firestore.js
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js';
-import { getFirestore, doc, setDoc, collection, addDoc, serverTimestamp } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js';
-import firebaseConfig from './firebaseConfig.js';
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db, doc, setDoc, collection, addDoc, serverTimestamp };
